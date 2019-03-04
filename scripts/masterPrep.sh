716071e8eec7bff4f65ca618fad0714ed6db817e
Pablo Sanchez <furybeat@gmail.com>
2019-03-04 15:13:47 +0100

    Update masterPrep.sh

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -75,6 +75,7 @@ subscription-manager repos \
     --enable="rhel-7-server-ose-3.11-rpms" \
     --enable="rhel-7-server-ansible-2.6-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
+    --enable="rhel-7-server-optional-rpms" \
     --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
 
 # Install base packages and update system to latest packages
